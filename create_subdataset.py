0cb5bbcf616db93fb58a5299edd923a1b05d7ba8
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-04-12 10:35:36 -0300

    Update create_subdataset.py

--- create_subdataset.py
+++ create_subdataset.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-
 df = pd.read_csv('sistema-unico-de-atencion-ciudadana-2021.csv', index_col=0, sep=";", low_memory=False)
 
-df_filtrado = df.loc[df['categoria'] == 'TRANSITO']
-df_filtrado.to_csv('trafico_en_festividades.csv', index=False)
+df['fecha_ingreso'] = pd.to_datetime(df['fecha_ingreso'])  
 
+mask = (df['fecha_ingreso'] > '1/12/2021') & (df['fecha_ingreso'] <= '31/12/2021')
+df_filtrado= df.loc[mask]
+df_filtrado.to_csv('filtrado.csv', index=False)
