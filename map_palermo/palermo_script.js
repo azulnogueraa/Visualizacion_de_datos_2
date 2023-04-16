b05239c2b4c6b195188f70c1ab4afc8ef8c92cb1
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-04-16 14:43:19 -0300

    Update palermo_script.js

--- map_palermo/palermo_script.js
+++ map_palermo/palermo_script.js
@@ -1,33 +1,39 @@
-// import addTooltips from "./addTooltips.js";
 const mapaFetch = d3.json('palermo-caba.geojson')
 const dataFetch = d3.dsv(';', 'dataset_seguridad_2022.csv', d3.autoType)
 
-Promise.all([mapaFetch, dataFetch]).then(([barrio, data]) => {
+Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
   
-    let chartMap = Plot.plot({
-      // https://github.com/observablehq/plot#projection-options
-      projection: {
-        type: 'mercator',
-        domain: barrio, // Objeto GeoJson a encuadrar
-      },
-      marks: [
-        Plot.geo(barrios, {
-          stroke: '#ddd',
-          title: d => `${d.properties.BARRIO}\n${d.properties.prestacion} prestación`,
-        }),
-        Plot.dot(data.filter(d => d.subcategoria === 'Mayor presencia policial'), { // Filtramos los datos solo para el barrio "Belgrano"
-  
-          x: 'lon',
-          y: 'lat',
-          r: 3,
-          stroke: 'none',
-          fill: 'black'
-        }),
-      ],
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+    marks: [
+      Plot.geo(barrios, {
+        stroke: 'black',
+        fill: '#152f67',
+        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
+        
+      }),
+
+      Plot.dot(data.filter(d => d.categoria === 'SEGURIDAD' && d.domicilio_barrio === 'PALERMO'), { // Filtramos los datos solo para el barrio "Belgrano"
+        x: 'lon',
+        y: 'lat',
+        r: 2,
+        stroke: 'none',
+        fill: 'black',
+      }),
+      Plot.image(data.filter(d => d.categoria === 'SEGURIDAD' && d.domicilio_barrio === 'PALERMO'), {
+        x: 'lon',
+        y: 'lat',
+        src: './police-car-light-svgrepo-com.svg'
+      })
       
-    })
-  
-    /* Agregamos al DOM la visualización chartMap */
-    d3.select('#chart').append(() => chartMap)
+    ],
+    
   })
-  
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart').append(() => chartMap)
+})
