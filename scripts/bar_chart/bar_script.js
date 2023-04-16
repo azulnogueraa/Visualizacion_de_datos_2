356f218d7b394220c7997013490c53b2f4acce66
rochigonzalezc <94115157+rochigonzalezc@users.noreply.github.com>
2023-04-16 20:42:00 -0300

    Update bar_script.js

--- scripts/bar_chart/bar_script.js
+++ scripts/bar_chart/bar_script.js
@@ -1,42 +1,38 @@
-Promise.all([
-    d3.dsv(';', 'dataset_seguridad_2020.csv', d3.autoType),
-    d3.dsv(';', 'dataset_seguridad_2021.csv', d3.autoType),
-    d3.dsv(';', 'dataset_seguridad_2022.csv', d3.autoType),
-    ]).then(([data2020, data2021, data2022]) => {
-    
-    // Unir los datos en una única estructura de datos
-    const data = [
-        ...data2020.map((d) => ({ ...d, year: "2020" })),
-        ...data2021.map((d) => ({ ...d, year: "2021" })),
-        ...data2022.map((d) => ({ ...d, year: "2022" })),
-      ];
+// config. fecha español
+d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
+  d3.timeFormatDefaultLocale(locale)
+})
 
-    // Trazar el gráfico de barras
-    let chart = Plot.plot({
-        marks: [
-        Plot.barY(data.filter((d) => d.year == "2020"), {
-            x: 'fecha_ingreso',
-            y: () => 1,
-        }),
-        Plot.barY(data.filter((d) => d.year == "2021"), {
-            x: 'fecha_ingreso',
-            y: () => 1,
-        }),
-        Plot.barY(data.filter((d) => d.year == "2022"), {
-            x: 'fecha_ingreso',
-            y: () => 1,
-        }),
-        ],
-        marginLeft: 200,
-        x: {
-        grid: true,
-        tickFormat: d3.format(',.0f'),
+Promise.all([
+    d3.dsv(';', 'dataset_2020.csv', d3.autoType),
+    d3.dsv(';', 'dataset_2021.csv', d3.autoType),
+    d3.dsv(';', 'dataset_2022.csv', d3.autoType),
+    ]).then(([data2020, data2021, data2022]) => {
+
+const data = [...data2020, ...data2021, ...data2022];
+  
+let chart = Plot.plot({  
+    marks: [
+    Plot.rectY(
+        data,
+        Plot.binX(
+        {
+            y: 'count',
+            title: d => JSON.stringify(d),
         },
-        y: {
-        label: '',
+        {
+            x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
+            thresholds: d3.timeYear,
         },
+        ),
+    ),
+    ],
+    y: {
+    grid: true,
+    label: 'Cantidad de reclamos',
+    tickFormat: 'd',
+    },
     })
-    
-    // Agregar el gráfico al div#chart en index.html
+    // Agregamos chart al div#chart de index.html
     d3.select('#chart').append(() => chart)
     })
