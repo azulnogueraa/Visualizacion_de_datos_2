55a422c3c88ec376413fb7d781d212868eb59bb5
rochigonzalezc <94115157+rochigonzalezc@users.noreply.github.com>
2023-04-16 21:01:55 -0300

    Update bar_script.js

--- scripts/bar_chart/bar_script.js
+++ scripts/bar_chart/bar_script.js
@@ -4,6 +4,7 @@ d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(
 })
 
 Promise.all([
+    //d3.dsv(';', 'dataset_2019.csv', d3.autoType),
     d3.dsv(';', 'dataset_2020.csv', d3.autoType),
     d3.dsv(';', 'dataset_2021.csv', d3.autoType),
     d3.dsv(';', 'dataset_2022.csv', d3.autoType),
@@ -22,9 +23,27 @@ let chart = Plot.plot({
         },
         {
             x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
+            thresholds: d3.timeMonth,
+        },
+    ),
+    ),
+    Plot.rectY(
+        data,
+        Plot.binX(
+        {
+            y: 'count',
+            title: d => JSON.stringify(d),
+        },
+        {
+            x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
             thresholds: d3.timeYear,
         },
-        ),
+        
+    ),
+    { 
+        opacity: 0.2,
+        fill: 'magenta',
+    }
     ),
     ],
     y: {
