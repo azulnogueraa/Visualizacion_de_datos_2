48b9e99368814d57f75ecaad71a52ed06309d6df
rochigonzalezc <94115157+rochigonzalezc@users.noreply.github.com>
2023-04-18 21:50:02 -0300

    Update bar_script.js

--- scripts/bar_chart/bar_script.js
+++ scripts/bar_chart/bar_script.js
@@ -3,106 +3,61 @@ d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(
   d3.timeFormatDefaultLocale(locale)
 })
 
-d3.dsv(';', '../../data/dataset_2020.csv', d3.autoType).then(data => {   
-let chart = Plot.plot({  
-    marks: [
-    Plot.rectY(
-        data,
-        Plot.binX(
-            {
-                y: 'count',
-                title: d => JSON.stringify(d),
-            },
-            {
-                x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
-                thresholds: d3.timeMonth,
-                fill: 'lightblue',
-                rx: 10,
-            },
-        ),
-    ),
-    ],
-    y: {
-    grid: true,
-    label: 'Cantidad de reclamos',
-    tickFormat: 'd',
-    },
-    })
-    // Agregamos chart al div#chart de index.html
-    d3.select('#chart').append(() => chart)
-    })    
-
-d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType).then(data => {   
-let chart = Plot.plot({  
-    marks: [
-    Plot.rectY(
-        data,
-        Plot.binX(
-            {
-                y: 'count',
-                title: d => JSON.stringify(d),
-            },
-            {
-                x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
-                thresholds: d3.timeMonth,
-                fill: 'lightblue',
-                rx: 10,
-            },
-        ),
-    ),
-    ],
-    y: {
-    grid: true,
-    label: 'Cantidad de reclamos',
-    tickFormat: 'd',
-    },
-    })
-    // Agregamos chart al div#chart de index.html
-    d3.select('#chart').append(() => chart)
-    })
-
-
 Promise.all([
-d3.dsv(';', '../../data/dataset_2020.csv', d3.autoType),
-d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType),
-]).then(([data2020, data2021]) => {
-    let chart = Plot.plot({
-        marks: [
-        Plot.rectY(data2020,
-            Plot.binX(
-                {
-                    y: 'count',
-                    title: d => JSON.stringify(d),
-                },
-                {
-                    x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
-                    thresholds: d3.timeMonth,
-                    fill: 'lightblue',
-                    rx: 10,
-                },
-            ),
+    d3.dsv(';', '../../data/dataset_2020.csv', d3.autoType),
+    d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType),
+    ]).then(([data2020, data2021]) => {
+
+const data = data2020.concat(data2021)
+  
+let chart = Plot.plot({  
+    marks: [
+    Plot.rectY(
+        data,
+        Plot.binX(
+            {
+                y: 'count',
+                title: d => JSON.stringify(d),
+            },
+            {
+                x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
+                thresholds: d3.timeMonth,
+                fill: 'lightblue',
+                rx: 10,
+            },
         ),
-        Plot.rectY(data2021,
-            Plot.binX(
-                {
-                    y: 'count',
-                    title: d => JSON.stringify(d),
-                },
-                {
-                    x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
-                    thresholds: d3.timeMonth,
-                    fill: 'green',
-                    rx: 10,
-                },
-            ),
-        ),
-        ],
-        y: {
-            grid: true,
-            label: 'Cantidad de reclamos',
-            tickFormat: 'd',
-        },
-    });
+    ),
+    ],
+    x: {
+        axis:null,
+    },
+    y: {
+    grid: true,
+    label: 'Cantidad de reclamos',
+    tickFormat: 'd',
+    },
+    facet: {
+        data: data,
+        x: d => d3.timeFormat('%B')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
+        facetConfig: {
+          marginBottom: 50,
+          facetPadding: 0.2,
+          facetMargins: { top: 10, right: 10, bottom: 50, left: 10 },
+          cell: {
+            fillOpacity: 0.8,
+            stroke: 'lightgray',
+            strokeWidth: 1,
+          },
+          widthRatio: 0.8,
+          // Configuración de ancho de faceta personalizado
+          width: (facetData) => {
+            const maxCount = d3.max(facetData, d => d.count);
+            const facetWidth = Math.max(20, maxCount * 5);
+            return facetWidth;
+          }
+        }
+      },
+    })
     // Agregamos chart al div#chart de index.html
-    d3.select('#chart').append(() => chart);
+    d3.select('#chart').append(() => chart)
 });
