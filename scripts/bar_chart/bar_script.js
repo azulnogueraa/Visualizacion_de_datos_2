8c46a118f23364a80676be99051f055bb3f15db9
rochigonzalezc <94115157+rochigonzalezc@users.noreply.github.com>
2023-04-21 19:43:56 -0300

    Update bar_script.js

--- scripts/bar_chart/bar_script.js
+++ scripts/bar_chart/bar_script.js
@@ -4,89 +4,35 @@ d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(
 })
 
 Promise.all([
-    d3.dsv(';', '../../data/dataset_2020.csv', d3.autoType),
-    d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType),
-    ]).then(([data2020, data2021]) => {
+  d3.dsv(';', '../../data/dataset_2020.csv', d3.autoType),
+  d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType),
+  ]).then(([data2020, data2021]) => {
 
-const data = data2020.concat(data2021)
-  
-let chart = Plot.plot({  
-    marks: [
-    Plot.rectY(
-        data,
-        Plot.binX(
-            {
-                y: 'count',
-                title: d => JSON.stringify(d),
-            },
-            {
-                x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
-                thresholds: d3.timeMonth,
-                fill: 'lightblue',
-                rx: 10,
-            },
-        ),
-    ),
+
+data = data2020.concat(data2021)
+
+let chart = Plot.plot({
+  marks: [
+    Plot.barY(data, {
+      x: d => d3.timeFormat('%b/%Y')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
+      y: () => 1,
+      fill: 'blue',
+      }),
     ],
-    x: {
-        axis:null,
-    },
-    y: {
-    grid: true,
-    label: 'Cantidad de reclamos',
-    tickFormat: 'd',
-    },
     facet: {
-        data: data,
-        x: d => d3.timeFormat('%B')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
-        facetConfig: {
-          marginBottom: 50,
-          facetPadding: 0.2,
-          facetMargins: { top: 10, right: 10, bottom: 50, left: 10 },
-          cell: {
-            fillOpacity: 0.8,
-            stroke: 'lightgray',
-            strokeWidth: 1,
-          },
-          widthRatio: 0.8,
-          // Configuración de ancho de faceta personalizado
-          width: (facetData) => {
-            const maxCount = d3.max(facetData, d => d.count);
-            const facetWidth = Math.max(20, maxCount * 5);
-            return facetWidth;
-          }
-        }
-      },
-    })
-    // Agregamos chart al div#chart de index.html
-    d3.select('#barchart').append(() => chart)
-});
-
-// d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType).then(data => {   
-// let chart = Plot.plot({  
-//     marks: [
-//     Plot.rectY(
-//         data,
-//         Plot.binX(
-//             {
-//                 y: 'count',
-//                 title: d => JSON.stringify(d),
-//             },
-//             {
-//                 x: d => d3.timeParse('%d/%m/%Y')(d.fecha_ingreso), 
-//                 thresholds: d3.timeMonth,
-//                 fill: 'lightblue',
-//                 rx: 10,
-//             },
-//         ),
-//     ),
-//     ],
-//     y: {
-//     grid: true,
-//     label: 'Cantidad de reclamos',
-//     tickFormat: 'd',
-//     },
-//     })
-//     // Agregamos chart al div#chart de index.html
-//     d3.select('#chart').append(() => chart)
-//     })
+      data: data,
+      x: d => d3.timeFormat('%m')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
+      label: "Meses",
+      labelAnchor: "center",
+    },  
+    // fx: {
+    //   domain: ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'],
+    // },
+    x: { axis: null, paddingInner: 0.1 },
+    y: {
+      label: "Cantidad de denuncias",
+      labelAnchor: "top",
+    },
+  })
+  d3.select('#chart').append(() => chart)
+})
