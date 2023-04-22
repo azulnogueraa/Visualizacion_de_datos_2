9d592ff20f0015430e91eb0117d64d0abc503fff
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-04-22 00:15:54 -0300

    Update coropletico.js

---
+++ scripts/map_chart/coropletico.js
@@ -0,0 +1,61 @@
+// config. fecha español
+d3.json('https://cdn.jsdelivr.net/npm/d3-time-format@3/locale/es-ES.json').then(locale => {
+  d3.timeFormatDefaultLocale(locale)
+})
+
+const coropletico = d3.json('../../data/barrios-caba.geojson')
+
+
+Promise.all([
+    coropletico,
+    d3.dsv(';', '../../data/dataset_2020.csv', d3.autoType),
+    d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType),
+    ]).then(([barrios, datos2020, datos2021]) => {
+  
+  datos = datos2020.concat(datos2021)
+
+  
+  const inseguridadPorBarrio = d3.group(datos, d => d.domicilio_barrio) // crea un Map
+  
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
+    },
+    color: {
+        // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+        type: 'quantize', 
+        n: 9,
+        scheme: 'blues',
+        label: 'grado de inseguridad',
+        legend: true,
+    },
+    marks: [
+      Plot.density(datos, { x: 'lon', y: 'lat', fill: 'density',bandwidth: 2, thresholds: 30 }),
+      Plot.geo(barrios, {
+        fill: d => {
+            let nombreBarrio = d.properties.BARRIO
+            let cantReclamos = (inseguridadPorBarrio.get(nombreBarrio).length)
+            return cantReclamos
+          },
+        stroke: 'gray',
+
+        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
+      }),
+    ],
+    facet: {
+        datos: datos.filter(d => d.fecha_ingreso.includes('/2020') || d.fecha_ingreso.includes('/2021')),
+        x: d => d3.timeFormat('%Y')(d3.timeParse('%d/%m/%Y')(d.fecha_ingreso)),
+
+    },
+    fx: {
+        domain: ['2020', '2021']
+    },
+    width: 1000
+  })
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart_coropletico').append(() => chartMap)
+})
