94f126bfae0174990b0567ec6e6fc33190f24569
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-04-25 15:38:04 -0300

    Update p2_script.js

--- scripts/palermo_chart/p2_script.js
+++ scripts/palermo_chart/p2_script.js
@@ -2,7 +2,9 @@ const mapaFetch_p2 = d3.json('../../data/palermo-caba.geojson')
 const dataFetch_p2 = d3.dsv(';', '../../data/dataset_2021.csv', d3.autoType)
 
 Promise.all([mapaFetch_p2, dataFetch_p2]).then(([barrios, data]) => {
-  
+
+  const inseguridadPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+
   let chartMap = Plot.plot({
     // https://github.com/observablehq/plot#projection-options
     projection: {
@@ -13,8 +15,7 @@ Promise.all([mapaFetch_p2, dataFetch_p2]).then(([barrios, data]) => {
       Plot.geo(barrios, {
         stroke: 'black',
         fill: '#074594',
-        title: d => `${d.properties.BARRIO}\n${d.properties.DENUNCIAS} denuncias`,
-        
+        title: d => `${d.properties.BARRIO}\n${inseguridadPorBarrio.get(d.properties.BARRIO)?.length || 0} reportes`,        
       }),
 
       Plot.dot(data.filter(d => d.categoria === 'SEGURIDAD' && d.domicilio_barrio === 'PALERMO'), { // Filtramos los datos solo para el barrio "Belgrano"
